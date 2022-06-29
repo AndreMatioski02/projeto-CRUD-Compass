887ed065f29851fe9e660495da6450542ccca58c
AndreMatioski02 <matioskiandre4@gmail.com>
2022-06-29 19:19:38 -0300

    Correção na máscara de CPF

--- app/src/view/cpf-mask.js
+++ app/src/view/cpf-mask.js
@@ -1,23 +1,26 @@
 function cpfMask(){
-    const inputCPF = document.querySelector('#inp-cpf')
+    const inputCPF = document.querySelectorAll('.cpfmask')
 
-    inputCPF.addEventListener('keypress', () => {
-        let inputLength = inputCPF.value.length
+    for(let i = 0; i < inputCPF.length; i++){
+        inputCPF[i].addEventListener('keypress', () => {
+            let inputLength = inputCPF[i].value.length
+    
+            if(inputLength === 3) {
+                inputCPF[i].value += '.'
+            } else if (inputLength === 7){
+                inputCPF[i].value += '.'
+            } else if (inputLength === 11){
+                inputCPF[i].value += '-'
+            }
+        });
+    
+        inputCPF[i].addEventListener('keypress', (event) => {
+            let keyCode = (event.keyCode ? event.keyCode : event.wich);
+    
+            if(keyCode < 47 || keyCode > 58){
+                event.preventDefault();
+            }
+        });
+    }
 
-        if(inputLength === 3) {
-            inputCPF.value += '.'
-        } else if (inputLength === 7){
-            inputCPF.value += '.'
-        } else if (inputLength === 11){
-            inputCPF.value += '-'
-        }
-    });
-
-    inputCPF.addEventListener('keypress', (event) => {
-        let keyCode = (event.keyCode ? event.keyCode : event.wich);
-
-        if(keyCode < 47 || keyCode > 58){
-            event.preventDefault();
-        }
-    });
 }
